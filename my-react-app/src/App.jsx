53e6a4b15dee3e54f76c9e1db4a2b618e07887cf
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-10-11 00:55:35 +0330

    second

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,8 @@
+import Introduction from "./task-2/Introduction";
+import "./App.css";
+
+function App() {
+  return <Introduction />;
+}
+
+export default App;
---
+++ my-react-app/src/task-2/Introduction.jsx
@@ -0,0 +1,9 @@
+import about from "../images/about.jpg";
+
+export default function Introduction() {
+  return (
+    <section>
+      <img src={about} alt="" className="img-intro" />{" "}
+    </section>
+  );
+}
